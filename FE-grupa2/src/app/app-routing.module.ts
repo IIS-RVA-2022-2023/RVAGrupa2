66689923e8579bea3c72d0b40a3a552e48e08887
Dejana Gladic <dejanagladic@ftn.ns>
2023-05-08 12:13:10 +0200

    Osmi čas - routing module

--- FE-grupa2/src/app/app-routing.module.ts
+++ FE-grupa2/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtiklComponent } from './components/artikl/artikl.component';
 
-const routes: Routes = [];
+const routes: Routes =  [ { path: 'artikl', component: ArtiklComponent }/*,   
+{ path: 'dobavljac', component: DobavljacComponent },
+{ path: 'porudzbina', component: PorudzbinaComponent },
+{ path: 'home', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+{ path: 'author', component: AuthorComponent },
+{ path: '', redirectTo: '/home', pathMatch: 'full'}*/
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
